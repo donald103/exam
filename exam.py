988b9589c1b3adfb41f2013d6da5250e6e46e017
donald103 <donyndadem@gmail.com>
2025-03-05 15:32:59 +0100

    Update exam.py

--- exam.py
+++ exam.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
+import streamlit as st
 df = pd.read_csv('data.csv',sep=";",encoding='cp1252') 
 # Étape 2: Afficher les premières lignes du DataFrame
 print("Premières lignes du DataFrame :")
